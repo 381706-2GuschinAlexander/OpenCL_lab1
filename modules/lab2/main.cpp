0f67e17e4b5bef509db4eb5e2e524101c7637d5f
Guschin Alexandr <igushin1999@gmail.com>
2020-11-10 11:40:48 +0300

    complete 2 lab

--- modules/lab2/main.cpp
+++ modules/lab2/main.cpp
@@ -15,7 +15,6 @@ int Type_Size(int type) {
 cl_int CL_Run(void* data, void* result, int count, int type, int group_size, void* fx, int incx, int incy) {
   const size_t size = count;
   
-
   cl_int error_code;
   cl_uint pl_count;
   if (clGetPlatformIDs(256, NULL, &pl_count) != CL_SUCCESS) {
@@ -29,12 +28,16 @@ cl_int CL_Run(void* data, void* result, int count, int type, int group_size, voi
   }
 
 
-  for (cl_uint i = 0; i < pl_count; ++i) {
-    char platformName[128];
-    clGetPlatformInfo(pl_id[i], CL_PLATFORM_NAME,
-      128, platformName, nullptr);
-    std::cout << platformName << std::endl;
-  }
+  //for (cl_uint i = 0; i < pl_count; ++i) {
+  //  char platformName[128];
+  //  clGetPlatformInfo(pl_id[i], CL_PLATFORM_NAME,
+  //    128, platformName, nullptr);
+  //  std::cout << platformName
+  //    << "\nCount =  " << count
+  //    << "\nGroup size = " << group_size
+  //    << "\nType = " << (type == 1 ? "float" : "double")
+  //    <<  std::endl;
+  //}
 
 
   cl_platform_id platform = pl_id[0];
@@ -96,7 +99,7 @@ cl_int CL_Run(void* data, void* result, int count, int type, int group_size, voi
   const char* csource = cont.c_str();
 
 
-  std::cout << cont;
+  //std::cout << cont;
 
   source.close();
 
@@ -124,7 +127,7 @@ cl_int CL_Run(void* data, void* result, int count, int type, int group_size, voi
 
   cl_kernel kernel = clCreateKernel(
     program,
-    (type ==1 ? "saxpy" : "daxpy"),
+    (type == 1 ? "saxpy" : "daxpy"),
     NULL);
 
   cl_mem input = clCreateBuffer(
@@ -153,6 +156,16 @@ cl_int CL_Run(void* data, void* result, int count, int type, int group_size, voi
     NULL,
     NULL);
 
+  clEnqueueWriteBuffer(
+    queue,
+    output,
+    CL_TRUE,
+    0,
+    Type_Size(type) * size,
+    result,
+    0,
+    NULL,
+    NULL);
 
   clSetKernelArg(
     kernel,
@@ -196,16 +209,9 @@ cl_int CL_Run(void* data, void* result, int count, int type, int group_size, voi
     &incy
   );
 
-  size_t group = 0;
+  size_t group = group_size;
 
-  clGetKernelWorkGroupInfo(
-    kernel,
-    device,
-    CL_KERNEL_WORK_GROUP_SIZE,
-    sizeof(size_t),
-    &group,
-    NULL);
-
+  auto start = omp_get_wtime();
   clEnqueueNDRangeKernel(
     queue,
     kernel,
@@ -216,6 +222,9 @@ cl_int CL_Run(void* data, void* result, int count, int type, int group_size, voi
     0,
     NULL,
     NULL);
+  clFinish(queue);
+  auto end = omp_get_wtime();
+  std::cout << (type == 1 ? "float gpu: " : "double gpu: ")<< end - start << std::endl;
 
   clEnqueueReadBuffer(
     queue,
@@ -228,12 +237,12 @@ cl_int CL_Run(void* data, void* result, int count, int type, int group_size, voi
     NULL,
     NULL);
 
-  if(type == 1)
+  /*if(type == 1)
     for (int i = 0; i < count; ++i)
       std::cout << (reinterpret_cast<float*>(data))[i] << "  " << (reinterpret_cast<float*>(result))[i] << "\n";
   else
     for (int i = 0; i < count; ++i)
-      std::cout << (reinterpret_cast<double*>(data))[i] << "  " << (reinterpret_cast<double*>(result))[i] << "\n";
+      std::cout << (reinterpret_cast<double*>(data))[i] << "  " << (reinterpret_cast<double*>(result))[i] << "\n";*/
 
   clReleaseMemObject(input);
   clReleaseMemObject(output);
@@ -257,49 +266,106 @@ cl_int CL_Run(void* data, void* result, int count, int type, int group_size, voi
 
 int main() {
   //int size = 1024;
-  int count = 1024;
+  int count = 128 * 1048576;
   int incx = 1, incy = 1;
 
-  float* X = new float[count];
-  float* Y = new float[count];
-
-  double* X_d = new double[count];
-  double* Y_d = new double[count];
+  float* CL_X = new float[count];
+  float* CL_Y = new float[count];
+  double* CL_X_d = new double[count];
+  double* CL_Y_d = new double[count];
 
   for (int i = 0; i < count; ++i) {
-    X[i] = i;
-    X_d[i] = i;
-    Y[i] = 2.1;
-    Y_d[i] = 2.1;
+    CL_X[i] = i % 100;
+    CL_X_d[i] = i % 100;
+    CL_Y[i] = 2.1;
+    CL_Y_d[i] = 2.1;
   }
   float fx = 3.1;
   double fx_d = 3.1;
 
-
-  CL_Run(X, Y, count, 1, 256, &fx, 1, 1);
   
-  CL_Run(X_d, Y_d, count, 2, 256, &fx_d, 1, 1);
+  CL_Run(CL_X, CL_Y, count, 1, 256, &fx, 1, 1);
+  
 
+  CL_Run(CL_X_d, CL_Y_d, count, 2, 256, &fx_d, 1, 1);
+  
+
+  float* OMP_X = new float[count];
+  float* OMP_Y = new float[count];
+  double* OMP_X_d = new double[count];
+  double* OMP_Y_d = new double[count];
 
   for (int i = 0; i < count; ++i) {
-    X[i] = i;
-    X_d[i] = i;
+    OMP_X[i] = i % 100;;
+    OMP_X_d[i] = i % 100;;
+    OMP_Y[i] = 2.1;
+    OMP_Y_d[i] = 2.1;
+  }
+
+  auto start = omp_get_wtime();
+  #pragma omp parallel for num_threads(6)
+  for (int i = 0; i < count; ++i)
+      OMP_Y[i] = OMP_Y[i] + fx * OMP_X[i];
+  auto end = omp_get_wtime();
+  std::cout << "float omp: " << end - start << std::endl;
+  
+  
+  start = omp_get_wtime();
+  #pragma omp parallel for num_threads(6)
+  for (int i = 0; i < count; ++i)
+    OMP_Y_d[i] = OMP_Y_d[i] + fx_d * OMP_X_d[i];
+  end = omp_get_wtime();
+  std::cout << "double omp: " << end - start << std::endl;
+
+
+
+  for (int i = 0; i < count; ++i)
+    if (abs(OMP_Y[i] - CL_Y[i]) > .001 || abs(OMP_Y_d[i] - CL_Y_d[i]) > .001)
+      printf("%d %f %f %lf %lf\n", i, OMP_Y[i], CL_Y[i], OMP_Y_d[i], CL_Y_d[i]);
+   
+  delete[] CL_X;
+  delete[] CL_Y;
+  delete[] CL_X_d;
+  delete[] CL_Y_d;
+
+  
+  float* X = new float[count];
+  float* Y = new float[count];
+  double* X_d = new double[count];
+  double* Y_d = new double[count];
+
+  for (int i = 0; i < count; ++i) {
+    X[i] = i % 100;;
+    X_d[i] = i % 100;;
     Y[i] = 2.1;
     Y_d[i] = 2.1;
   }
 
-  /*#pragma omp parallel for num_threads(4)
-  {
-    for(int i = 0; i < count; ++i)
-      
-  }*/
-  //for (int i = 0; i < count; ++i)
-  //  std::cout <<Y[i] << "\n";
+  start = omp_get_wtime();
+  for (int i = 0; i < count; ++i)
+    Y[i] = Y[i] + fx * X[i];
+  end = omp_get_wtime();
+  std::cout << "float seq: " << end - start << std::endl;
+
+  start = omp_get_wtime();
+  for (int i = 0; i < count; ++i)
+    Y_d[i] = Y_d[i] + fx_d * X_d[i];
+  end = omp_get_wtime();
+  std::cout << "double seq: " << end - start << std::endl;
+
+  for (int i = 0; i < count; ++i)
+    if (abs(OMP_Y[i] - Y[i]) > .001 || abs(OMP_Y_d[i] - Y_d[i]) > .001)
+      printf("%d %f %f %lf %lf\n", i, OMP_Y[i], Y[i], OMP_Y_d[i], Y_d[i]);
 
   delete[] X;
   delete[] Y;
   delete[] X_d;
   delete[] Y_d;
+
+  delete[] OMP_X;
+  delete[] OMP_Y;
+  delete[] OMP_X_d;
+  delete[] OMP_Y_d;
 
   return 0;
 }
